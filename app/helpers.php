1224e45c50e39b71bfd0b468033b33bb88e0fcb6
Tudor <tgoncear@gmail.com>
2021-10-26 17:14:15 +0200

    He afegit la funcion connectDB()

--- app/helpers.php
+++ app/helpers.php
@@ -7,3 +7,13 @@ function dd($param){
     var_dump($param);
     die();
 }
+function connectDB($config){
+    try{
+        $user = $config['database']['user'];
+        $pass = $config['database']['password'];
+        $dsn = "{$config['database']['databasetype']}:host={$config['database']['host']};dbname={$config['database']['databasename']}";
+        return new PDO($dsn,$user,$pass);
+    }catch(Exception $e){
+        echo 'Error de connexio a la base de dades';
+    }
+}
--- app/index.php
+++ app/index.php
@@ -1,19 +1,13 @@
 <?php
+require 'config.php';
 require "helpers.php";
 require "Task.php";
 //$user = 'debian-sys-maint';
 //$pass = 'YkXqIXFQIkDtMNPs';
 //$dsn = 'mysql:host=localhost;dbname=phpLaravelDevs';
-require 'config.php';
-$user = $config['database']['user'];
-$pass = $config['database']['password'];
-$dsn = "{$config['database']['databasetype']}:host={$config['database']['host']};dbname={$config['database']['databasename']}";
 
-try{
-    $dbh = new PDO($dsn,$user,$pass);
-}catch(Exception $e){
-    echo 'Error de connexio a la base de dades';
-}
+$dbh = connectDB($config);
+
 $statement = $dbh->prepare('SELECT * FROM tasks');
 $statement->execute();
 $tasks = $statement->fetchAll(PDO::FETCH_CLASS,'Task');
